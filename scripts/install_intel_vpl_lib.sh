5e18ad0bc400b745d668bf2e45b9fcac17313543
Tom Teasdale <teasdale@lightsaber.red>
2026-08-02 11:41:49 +0200

    Update install_intel_vpl_lib.sh

--- scripts/install_intel_vpl_lib.sh
+++ scripts/install_intel_vpl_lib.sh
@@ -2,7 +2,23 @@
 # https://github.com/intel/libvpl/blob/main/INSTALL.md
 cd ./libvpl
 export VPL_INSTALL_DIR=`pwd`/../_vplinstall
-. ./script/bootstrap
+
+# Install only the dependencies needed to build libvpl.
+# Avoid running libvpl/script/bootstrap here because it reinstalls distro
+# libva packages and overwrites the custom libva runtime installed earlier.
+apt-get update
+apt-get install -y --no-install-recommends \
+	build-essential \
+	cmake \
+	pkg-config \
+	libdrm-dev \
+	libx11-dev \
+	libx11-xcb-dev \
+	libxcb-present-dev \
+	libxcb-dri3-dev \
+	libwayland-dev \
+	wayland-protocols
+
 rm -rf _build
 cmake -B _build -DCMAKE_INSTALL_PREFIX=$VPL_INSTALL_DIR
 cmake --build _build
