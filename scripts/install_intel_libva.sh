6d8b40442ca47650691d4a09d28cb906e482d5fd
Tom Teasdale <teasdale@lightsaber.red>
2026-08-02 02:09:31 +0200

    Add intel-media-va-driver and vainfo deps
    Include intel-media-va-driver-non-free and vainfo in the libva install script to provide the Intel VA-API driver and the vainfo diagnostic tool.

--- scripts/install_intel_libva.sh
+++ scripts/install_intel_libva.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # https://github.com/intel/libva#build-and-install-libva
-apt-get install -y git cmake pkg-config meson libdrm-dev automake libtool
+apt-get install -y git cmake pkg-config meson libdrm-dev automake libtool intel-media-va-driver-non-free vainfo
 cd libva
 ./autogen.sh --prefix=/usr --libdir=/usr/lib/x86_64-linux-gnu
 make
